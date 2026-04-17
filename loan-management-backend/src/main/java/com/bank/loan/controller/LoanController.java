074c1bce949c3114ecaff95f7d28fab64a6941cd
Omkar Naik <naikomkar5428@gmail.com>
2026-04-17 20:58:35 +0530

    fix: allow all CORS origins for production

--- loan-management-backend/src/main/java/com/bank/loan/controller/LoanController.java
+++ loan-management-backend/src/main/java/com/bank/loan/controller/LoanController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
+@CrossOrigin(origins = "*")
 @RequestMapping("/loan")
 public class LoanController {
 
